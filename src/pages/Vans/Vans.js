86cf14c1c8f800a050bcac1c03e11d2e28a1bf57
Ivan <arias.ivan@gmail.com>
2023-07-05 12:08:50 -0400

    Actions and Protected Routes

--- src/pages/Vans/Vans.js
+++ src/pages/Vans/Vans.js
@@ -1,17 +1,17 @@
 import React from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { Link, useSearchParams, useLoaderData } from "react-router-dom"
+import { getVans } from "../../api"
+
+export function loader() {
+    return getVans()
+}
 
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams()
-    const [vans, setVans] = React.useState([])
+    const [error, setError] = React.useState(null)
+    const vans = useLoaderData()
 
     const typeFilter = searchParams.get("type")
-
-    React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
 
     const displayedVans = typeFilter
         ? vans.filter(van => van.type === typeFilter)
@@ -19,11 +19,11 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-              <Link 
-                to={van.id} 
-                state={{ 
-                    search: `?${searchParams.toString()}`, 
-                    type: typeFilter 
+            <Link
+                to={van.id}
+                state={{
+                    search: `?${searchParams.toString()}`,
+                    type: typeFilter
                 }}
             >
                 <img src={van.imageUrl} />
@@ -45,6 +45,10 @@ export default function Vans() {
             }
             return prevParams
         })
+    }
+    
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
     }
 
     return (
